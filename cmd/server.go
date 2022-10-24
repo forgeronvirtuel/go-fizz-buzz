d44bf9e3aa536724f5b4ae45396b543f92960a37
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-24 15:41:03 +0200

    dev(fizzbuzz): transforming into REST API

--- cmd/server.go
+++ cmd/server.go
@@ -8,16 +8,6 @@ import (
 	"strings"
 )
 
-const (
-	limitname         = "limit"
-	int1name          = "int1"
-	int2name          = "int1"
-	string1name       = "str1"
-	string2name       = "str2"
-	formatIntvalue    = "%s = %d"
-	formatStringvalue = "%s = %s"
-)
-
 func main() {
 	router := setupRouter()
 	if err := router.Run("localhost:9080"); err != nil {
@@ -27,23 +17,25 @@ func main() {
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-	int1 := 0
-	int2 := 0
-	limit := 0
-	s1 := ""
-	s2 := ""
-	router.GET("/limit", createRouteGetInt(limitname, &limit))
-	router.PUT("/limit", createRoutePutInt(&limit))
-	router.GET("/int/first", createRouteGetInt(int1name, &int1))
-	router.PUT("/int/first", createRoutePutInt(&int1))
-	router.GET("/int/second", createRouteGetInt(int2name, &int2))
-	router.PUT("/int/second", createRoutePutInt(&int2))
-	router.GET("/string/first", createRouteGetString(string1name, &s1))
-	router.PUT("/string/first", createRoutePutString(&s1))
-	router.GET("/string/second", createRouteGetString(string2name, &s2))
-	router.PUT("/string/second", createRoutePutString(&s2))
-	router.GET("/result", fizzbuzz(&int1, &int2, &limit, &s1, &s2))
+	router.GET("/", fizzbuzzRoute)
 	return router
+}
+
+type fizzBuzzParam struct {
+	Int1  int    `form:"int1" binding:"required,min=1"`
+	Int2  int    `form:"int2" binding:"required,min=1"`
+	Limit int    `form:"limit" binding:"required,min=1"`
+	Str1  string `form:"str1" binding:"required"`
+	Str2  string `form:"str2" binding:"required"`
+}
+
+func fizzbuzzRoute(c *gin.Context) {
+	var params fizzBuzzParam
+	if err := c.Bind(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.String(200, fizzbuzz(params.Int1, params.Int2, params.Limit, params.Str1, params.Str2))
 }
 
 func format(sb *strings.Builder, i1, i2, i int, s1, s2, concat string) {
@@ -58,59 +50,13 @@ func format(sb *strings.Builder, i1, i2, i int, s1, s2, concat string) {
 	}
 }
 
-func fizzbuzz(i1, i2, limit *int, s1, s2 *string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var sb strings.Builder
-		concat := fmt.Sprintf("%s%s", *s1, *s2)
-		for i := 1; i < *limit; i++ {
-			format(&sb, *i1, *i2, i, *s1, *s2, concat)
-			sb.WriteRune(',')
-		}
-		format(&sb, *i1, *i2, *limit, *s1, *s2, concat)
-		c.String(http.StatusOK, sb.String())
+func fizzbuzz(i1, i2, limit int, s1, s2 string) string {
+	var sb strings.Builder
+	concat := fmt.Sprintf("%s%s", s1, s2)
+	for i := 1; i < limit; i++ {
+		format(&sb, i1, i2, i, s1, s2, concat)
+		sb.WriteRune(',')
 	}
-}
-
-type stringValue struct {
-	Value string `json:"value" binding:"required"`
-}
-
-func createRoutePutString(value *string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		body := stringValue{}
-		if err := c.ShouldBindJSON(&body); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err})
-			return
-		}
-		*value = body.Value
-		c.JSON(http.StatusOK, &body)
-	}
-}
-
-func createRouteGetString(varname string, value *string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.String(http.StatusOK, formatStringvalue, varname, *value)
-	}
-}
-
-type intValue struct {
-	Value int `json:"value" binding:"required,min=1"`
-}
-
-func createRoutePutInt(value *int) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		body := intValue{}
-		if err := c.ShouldBindJSON(&body); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err})
-			return
-		}
-		*value = body.Value
-		c.Status(http.StatusOK)
-	}
-}
-
-func createRouteGetInt(varname string, value *int) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.String(http.StatusOK, formatIntvalue, varname, *value)
-	}
+	format(&sb, i1, i2, limit, s1, s2, concat)
+	return sb.String()
 }
--- cmd/server_test.go
+++ cmd/server_test.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -15,112 +12,7 @@ func TestSetFirstValue(t *testing.T) {
 
 	// Read the first int, default value
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/int/first", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatIntvalue, int1name, 0), w.Body.String())
-
-	// Read the second int, default value
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/int/second", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatIntvalue, int2name, 0), w.Body.String())
-
-	// Read the limit, default value
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/limit", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatIntvalue, limitname, 0), w.Body.String())
-
-	// Read the first string, default value
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/string/first", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatStringvalue, string1name, ""), w.Body.String())
-
-	// Read the second string, default value
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/string/second", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatStringvalue, string2name, ""), w.Body.String())
-
-	// Set the first value to 3
-	bodyInt := intValue{Value: 3}
-	v, _ := json.Marshal(bodyInt)
-	req, _ = http.NewRequest(http.MethodPut, "/int/first", bytes.NewReader(v))
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-
-	// Set the second value to 5
-	bodyInt = intValue{Value: 5}
-	v, _ = json.Marshal(bodyInt)
-	req, _ = http.NewRequest(http.MethodPut, "/int/second", bytes.NewReader(v))
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-
-	// Set the limit to 15
-	bodyInt = intValue{Value: 15}
-	v, _ = json.Marshal(bodyInt)
-	req, _ = http.NewRequest(http.MethodPut, "/limit", bytes.NewReader(v))
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-
-	// Set the first string value to fizz
-	bodyStr := stringValue{Value: "fizz"}
-	v, _ = json.Marshal(bodyStr)
-	req, _ = http.NewRequest(http.MethodPut, "/string/first", bytes.NewReader(v))
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-
-	// Set the second string value to fizz
-	bodyStr = stringValue{Value: "buzz"}
-	v, _ = json.Marshal(bodyStr)
-	req, _ = http.NewRequest(http.MethodPut, "/string/second", bytes.NewReader(v))
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-
-	// Read the first int
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/int/first", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatIntvalue, int1name, 3), w.Body.String())
-
-	// Read the second int
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/int/second", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatIntvalue, int2name, 5), w.Body.String())
-
-	// Read the limit
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/limit", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatIntvalue, limitname, 15), w.Body.String())
-
-	// Read the first string
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/string/first", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatStringvalue, string1name, "fizz"), w.Body.String())
-
-	// Read the second string
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/string/second", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatStringvalue, string2name, "buzz"), w.Body.String())
-
-	// Testing fizz buzz
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest(http.MethodGet, "/result", nil)
+	req, _ := http.NewRequest(http.MethodGet, "/?int1=3&int2=5&limit=15&str1=fizz&str2=buzz", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz", w.Body.String())
