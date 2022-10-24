708f00bf16595ed65d14113daa19d8e9e169bca6
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-24 14:09:23 +0200

    dev(int1, int2): save value for str1 and str2

--- cmd/server.go
+++ cmd/server.go
@@ -7,9 +7,12 @@ import (
 )
 
 const (
-	int1name    = "int1"
-	int2name    = "int1"
-	formatvalue = "%s = %d"
+	int1name          = "int1"
+	int2name          = "int1"
+	string1name       = "str1"
+	string2name       = "str2"
+	formatIntvalue    = "%s = %d"
+	formatStringvalue = "%s = %s"
 )
 
 func main() {
@@ -23,11 +26,39 @@ func setupRouter() *gin.Engine {
 	router := gin.Default()
 	int1 := 0
 	int2 := 0
+	s1 := ""
+	s2 := ""
 	router.GET("/int/first", createRouteGetInt(int1name, &int1))
 	router.PUT("/int/first", createRoutePutInt(&int1))
 	router.GET("/int/second", createRouteGetInt(int2name, &int2))
 	router.PUT("/int/second", createRoutePutInt(&int2))
+	router.GET("/string/first", createRouteGetString(string1name, &s1))
+	router.PUT("/string/first", createRoutePutString(&s1))
+	router.GET("/string/second", createRouteGetString(string2name, &s2))
+	router.PUT("/string/second", createRoutePutString(&s2))
 	return router
+}
+
+type stringValue struct {
+	Value string `json:"value" binding:"required"`
+}
+
+func createRoutePutString(value *string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		body := stringValue{}
+		if err := c.ShouldBindJSON(&body); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		*value = body.Value
+		c.JSON(http.StatusOK, &body)
+	}
+}
+
+func createRouteGetString(varname string, value *string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.String(http.StatusOK, formatStringvalue, varname, *value)
+	}
 }
 
 type intValue struct {
@@ -48,6 +79,6 @@ func createRoutePutInt(value *int) gin.HandlerFunc {
 
 func createRouteGetInt(varname string, value *int) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.String(http.StatusOK, formatvalue, varname, *value)
+		c.String(http.StatusOK, formatIntvalue, varname, *value)
 	}
 }
--- cmd/server_test.go
+++ cmd/server_test.go
@@ -18,41 +18,82 @@ func TestSetFirstValue(t *testing.T) {
 	req, _ := http.NewRequest(http.MethodGet, "/int/first", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatvalue, int1name, 0), w.Body.String())
+	assert.Equal(t, fmt.Sprintf(formatIntvalue, int1name, 0), w.Body.String())
 
 	// Read the second int, default value
 	w = httptest.NewRecorder()
 	req, _ = http.NewRequest(http.MethodGet, "/int/second", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatvalue, int2name, 0), w.Body.String())
+	assert.Equal(t, fmt.Sprintf(formatIntvalue, int2name, 0), w.Body.String())
+
+	// Read the first string, default value
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodGet, "/string/first", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, fmt.Sprintf(formatStringvalue, string1name, ""), w.Body.String())
+
+	// Read the second string, default value
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodGet, "/string/second", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, fmt.Sprintf(formatStringvalue, string2name, ""), w.Body.String())
 
 	// Set the first value to 3
-	body := intValue{Value: 3}
-	v, _ := json.Marshal(body)
+	bodyInt := intValue{Value: 3}
+	v, _ := json.Marshal(bodyInt)
 	req, _ = http.NewRequest(http.MethodPut, "/int/first", bytes.NewReader(v))
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 
 	// Set the second value to 5
-	body = intValue{Value: 5}
-	v, _ = json.Marshal(body)
+	bodyInt = intValue{Value: 5}
+	v, _ = json.Marshal(bodyInt)
 	req, _ = http.NewRequest(http.MethodPut, "/int/second", bytes.NewReader(v))
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// Read the first int, default value
+	// Set the first string value to fizz
+	bodyStr := stringValue{Value: "fizz"}
+	v, _ = json.Marshal(bodyStr)
+	req, _ = http.NewRequest(http.MethodPut, "/string/first", bytes.NewReader(v))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// Set the second string value to fizz
+	bodyStr = stringValue{Value: "buzz"}
+	v, _ = json.Marshal(bodyStr)
+	req, _ = http.NewRequest(http.MethodPut, "/string/second", bytes.NewReader(v))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	// Read the first int
 	w = httptest.NewRecorder()
 	req, _ = http.NewRequest(http.MethodGet, "/int/first", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatvalue, int1name, 3), w.Body.String())
+	assert.Equal(t, fmt.Sprintf(formatIntvalue, int1name, 3), w.Body.String())
 
-	// Read the second int, default value
+	// Read the second int
 	w = httptest.NewRecorder()
 	req, _ = http.NewRequest(http.MethodGet, "/int/second", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, fmt.Sprintf(formatvalue, int2name, 5), w.Body.String())
+	assert.Equal(t, fmt.Sprintf(formatIntvalue, int2name, 5), w.Body.String())
 
+	// Read the first string
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodGet, "/string/first", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, fmt.Sprintf(formatStringvalue, string1name, "fizz"), w.Body.String())
+
+	// Read the second string
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodGet, "/string/second", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, fmt.Sprintf(formatStringvalue, string2name, "buzz"), w.Body.String())
 }
